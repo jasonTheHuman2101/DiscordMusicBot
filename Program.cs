89a80ec64d912a98402d6bb2a44456697083dd59
Jason Chesters-salt <43147625+jasonTheHuman2101@users.noreply.github.com>
2022-09-03 19:43:34 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Discord;
+
+namespace DiscordMusicBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Music Bot!");
+        }
+    }
+}
